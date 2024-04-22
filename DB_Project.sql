ac8b89477b63dc888bd14ae08b248aaa0adb4e55
A-Goss <125573542+A-Goss@users.noreply.github.com>
2024-04-21 22:59:17 -0500

    Newest Version of Code with input

--- DB_Project.sql
+++ DB_Project.sql
@@ -3,7 +3,7 @@ CREATE DATABASE DB_Project;
 USE DB_Project;
 
 Create TABLE Customer(
-	Customer_ID int (11) NOT NULL,
+	Customer_ID int  NOT NULL,
     Gender char(2) NOT NULL,
     Email varchar(20) NOT NULL,
     Fname varchar(20) NOT NULL,
@@ -14,37 +14,37 @@ Create TABLE Customer(
 );
 
 Create TABLE Employee(
-	Employee_ID int (11) NOT NULL,
-	Location_ID varchar(20) NOT NULL,
+	Employee_ID int  NOT NULL,
+	Location_ID int NOT NULL,
     State varchar(20) NOT NULL,
     City varchar(20) NOT NULL,
     Address varchar(20) NOT NULL,
     Employee_Desc varchar(40) NOT NULL,
-    Primary Key (Employee_ID) NOT NULL,
-    Foreign Key (Loaction_ID) REFERENCES Location(Loaction_ID)
+    Primary Key (Employee_ID),
+    Foreign Key (Location_ID) REFERENCES Location(Location_ID)
 );
 
 Create TABLE Location(
-	Location_ID int(11) NOT NULL,
+	Location_ID int NOT NULL,
     Address varchar(20) NOT NULL,
     Phone_Number char(10) NOT NULL,
     Primary Key (Location_ID)
 );
 
 Create TABLE Products(
-	Product_ID int (11) NOT NULL,
+	Product_ID int  NOT NULL,
     Prod_Description varchar (100) NOT NULL,
     Quantity int (5) NOT NULL,
     primary key (Product_ID)
 );
 
 Create TABLE Purchase_detail(
-	Transaction_ID int(11) NOT NULL ,
-    Product_ID int (11) NOT NULL,
-    Product_Quantity int (4) NOT NULL,
+	Transaction_ID int NOT NULL ,
+    Product_ID int  NOT NULL,
+    Product_Quantity int  NOT NULL,
     Product_Price float(6) NOT NULL,
-    Customer_ID int (11) NOT NULL,
-    Employee_ID int (11) NOT NULL,
+    Customer_ID int  NOT NULL,
+    Employee_ID int  NOT NULL,
     Primary Key (Transaction_ID),
     Foreign Key (Product_ID) REFERENCES Products(Product_ID),
     Foreign Key (Customer_ID) REFERENCES Customer(Customer_ID),
@@ -52,10 +52,10 @@ Create TABLE Purchase_detail(
 );
 
 Create TABLE Delivery(
-	Delivery_ID int(11) NOT NULL,
-    Employee_ID int (11) NOT NULL,
-    Location_ID int (11) NOT NULL,
-    Transaction_ID int (11) NOT NULL,
+	Delivery_ID int NOT NULL,
+    Employee_ID int NOT NULL,
+    Location_ID int NOT NULL,
+    Transaction_ID int NOT NULL,
     Address varchar (50) NOT NULL,
     Primary key (Delivery_ID, Employee_ID, Location_ID),
     Foreign Key (Employee_ID) REFERENCES Employee(Employee_ID),
@@ -64,13 +64,26 @@ Create TABLE Delivery(
 );
 
 Create TABLE Checkout(
-	Checkout_ID int (11) NOT NULL,
-    Transaction_ID int (11) NOT NULL,
+	Checkout_ID int NOT NULL,
+    Transaction_ID int NOT NULL,
     Checkout_Date varchar (8) NOT NULL,
     Amount float (6) NOT NULL,
     Payment_Method varchar (20) NOT NULL,
-	Location_ID int (11) NOT NULL,
+	Location_ID int NOT NULL,
 	Primary key (Checkout_ID,Location_ID),
     Foreign Key (Location_ID) REFERENCES Location(Location_ID),
 	Foreign Key (Transaction_ID) REFERENCES Purchase_detail(Transaction_ID)
 );
+
+
+/* loading values into tables /*
+
+/* Customer values */
+insert into Customer values (103869,'M','demoarch@gmail.com','Derrak','Johnson',2346459834,'243 CourtHouse Ct');
+insert into Customer values (245689,'F','girlyfin@gmail.com','Hannah','Fleaker',2349887670,'300 FamilyRight St');
+insert into Customer values (986532,'M','dMan@gmail.com','Danny','Phantom',2341236770,'543 CandyLane St');
+
+
+Select *
+From Customer;
+
