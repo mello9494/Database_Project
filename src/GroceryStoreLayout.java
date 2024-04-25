2c155e45dec9c18b7bb854800258e451cd6daff8
Tanner <tangolfer94@gmail.com>
2024-04-25 17:15:23 -0500

    Added functionality to buttons.

--- src/GroceryStoreLayout.java
+++ src/GroceryStoreLayout.java
@@ -2,10 +2,14 @@ import com.mysql.cj.xdevapi.Table;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Objects;
 
 public class GroceryStoreLayout extends JFrame{
@@ -46,6 +50,7 @@ public class GroceryStoreLayout extends JFrame{
     private JLabel selectIDText;
     private JLabel[] labels = {label1, label2, label3, label4, label5, label6, label7, label8, label9, label10};
     private JTextField[] textFields= {textField1, textField2, textField3, textField4, textField5, textField6, textField7, textField8, textField9, textField10};
+    private JDialog dialog = new JDialog(this, "");;
 
     public Connection con;
     public ResultSet result;
@@ -54,6 +59,8 @@ public class GroceryStoreLayout extends JFrame{
     public String selectedID;
     public int numColumns;
     DefaultTableModel tableModel;
+    Dictionary<String, Integer> dataTypes = new Hashtable<>(); // data types of getColumnType returned numbers
+
 
     GroceryStoreLayout(){
         setContentPane(frame);
@@ -133,11 +140,31 @@ public class GroceryStoreLayout extends JFrame{
         });
 
 
-    }
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createButton();
+                generateTable();
+            }
+        });
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
 
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateButton();
+                generateTable();
+            }
+        });
+
+
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                deleteButton();
+                generateTable();
+            }
+        });
     }
 
     public void generateTable(){
@@ -188,15 +215,6 @@ public class GroceryStoreLayout extends JFrame{
         }
     }
 
-    public int getRowCount(String table){
-        try {
-            queryDb(con, "select count(*) from " + table);
-            return Integer.parseInt(result.getString(1));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public void setID_selectorItems(){
         ID_selector.removeAllItems();
         ID_selector.addItem("All");
@@ -224,15 +242,127 @@ public class GroceryStoreLayout extends JFrame{
         }
     }
 
-    public void setTextFieldtext(){
+    public void setTextFieldtext(){ // set the text fields when an ID is selected
         try {
             int i = 0;
             while(result.next()){
-                textFields[i].setText(result.getString(1)); ;
+                textFields[i].setText(result.getString(1));
+                i++;
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void createButton(){
+        try {
+            // get table name
+            //get column names
+            StringBuilder statement = new StringBuilder("insert into $tablename values (");
+            PreparedStatement newPreparedStatement;
+            int columnType;
+            for(int i = 0; i < numColumns; i++){ // add insert options for every column needed in table
+                if(i == numColumns - 1){ statement.append("?);"); }
+                else{ statement.append("?, "); }
+            }
+
+            statement.replace(12, 22, table_selector.getSelectedItem().toString());
+            newPreparedStatement = con.prepareStatement(statement.toString());
+            queryDb(con, "select * from " + table_selector.getSelectedItem().toString());
+
+            for(int i = 1; i <= numColumns; i++){
+                columnType = result.getMetaData().getColumnType(i);
+                // get data type of table element
+                if(columnType == 1 || columnType == 12){ // char or varchar
+                    newPreparedStatement.setString(i, textFields[i - 1].getText());
+                }
+                else if(columnType == 4){ // int
+                    newPreparedStatement.setInt(i, Integer.parseInt(textFields[i - 1].getText()));
+                }
+                else if(columnType == 6){ // float
+                    newPreparedStatement.setFloat(i, Float.parseFloat(textFields[i - 1].getText()));
+                }
+            }
+            int rows = newPreparedStatement.executeUpdate();
+            if(rows > 0){
+                System.out.println("Add success!");
+            }
+
+        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+            // show popup window with correct error
+            errorMessageDialogBox("Too long", e);
+        }
+    }
+
+    public void updateButton(){
+        try {
+            StringBuilder statement = new StringBuilder("update $tablename set ");
+            PreparedStatement newPreparedStatement;
+            int columnType;
+            int rows;
+            String colName;
+            for(int i = 0; i < numColumns; i++){ // add insert options for every column needed in table
+                colName = tableModel.getColumnName(i);
+                if(i == numColumns - 1){ statement.append(colName).append(" = ?"); }
+                else{ statement.append(colName).append(" = ?, "); }
+            }
+
+            statement.append("where ").append(tableModel.getColumnName(0)).append(" = ").append(tableModel.getValueAt(Table_Data.getSelectedRow(), 0)); // only update the selected row
+            statement.replace(7, 17, table_selector.getSelectedItem().toString());
+            newPreparedStatement = con.prepareStatement(statement.toString());
+            queryDb(con, "select * from " + table_selector.getSelectedItem().toString()); // to get the column types
+
+            for(int i = 1; i <= numColumns; i++){
+                columnType = result.getMetaData().getColumnType(i);
+                // get data type of table element
+                if(columnType == 1 || columnType == 12){ // char or varchar
+                    newPreparedStatement.setString(i, textFields[i - 1].getText());
+                }
+                else if(columnType == 4){ // int
+                    newPreparedStatement.setInt(i, Integer.parseInt(textFields[i - 1].getText()));
+                }
+                else if(columnType == 6){ // float
+                    newPreparedStatement.setFloat(i, Float.parseFloat(textFields[i - 1].getText()));
+                }
+            }
+            rows = newPreparedStatement.executeUpdate();
+            if(rows > 0){
+                System.out.println("Update success!");
+            }
+            dialogBox("Updated Successfully");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteButton(){
+        StringBuilder statement = new StringBuilder("delete from $tablename where ");
+        PreparedStatement newPreparedStatement;
+
+        statement.replace(12, 22, table_selector.getSelectedItem().toString());
+        statement.append(tableModel.getColumnName(0)).append(" = ").append(tableModel.getValueAt(Table_Data.getSelectedRow(), 0)); // only delete the selected row
+        try {
+            newPreparedStatement = con.prepareStatement(statement.toString());
+            newPreparedStatement.executeUpdate();
+            dialogBox("Deleted Successfully");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void dialogBox(String message){
+        dialog.setTitle("Success");
+        JLabel newLabel = new JLabel();
+        newLabel.setText(message);
+        dialog.setSize(300, 100);
+        dialog.setLocationRelativeTo(this);
+        dialog.add(newLabel);
+        dialog.setVisible(true);
+    }
+
+    public void errorMessageDialogBox(String message, SQLException e){
+
     }
 
     public static void main(String[] args) {
